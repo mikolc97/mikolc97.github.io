2ecab66de93f24d7c9067a06325712e7d5afd728
mikolc97 <156839387+mikolc97@users.noreply.github.com>
2024-03-01 18:19:49 +0100

    Add files via upload
    Portfolio

---
+++ script.js
@@ -0,0 +1,17 @@
+function scrollToElement(elementSelector, instance=0){
+const elements = document.querySelectorAll(elementSelector);
+if(elements.length >instance){
+
+    elements[instance].scrollIntoView({behavior:'smooth'});
+}
+}
+
+const link1= document.getElementById("link1");
+const link2= document.getElementById("link2");
+
+link1.addEventListener('click', () => {
+    scrollToElement('.container',1)
+})
+link2.addEventListener('click', () => {
+    scrollToElement('.container',3)
+})
